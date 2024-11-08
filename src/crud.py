7493649d8347a64ffc55e107afe409277b2b9b65
Mark <markbakalkinsochi3@gmail.com>
2024-11-08 13:32:52 +0300

    add sync_session in crud.py

--- src/crud.py
+++ src/crud.py
@@ -6,30 +6,26 @@ Delete
 '''
 
 from sqlalchemy import select, update, insert, delete
-from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
 from fastapi import Depends
 
-from typing import Annotated
-
-from .database import create_async_engine
+from .database import async_session
 from .models.user_and_role import User
 from .auth.schemas import UserCreate
 
 
-session_db = Annotated[async_sessionmaker, Depends(create_async_engine)]
-
-
 #CREATE
-async def create_user(user_in: UserCreate, session: AsyncSession = session_db) -> dict:
-    stmt = insert(User).values(**(user_in.model_dump()))
-    await session.execute(stmt)
-    await session.commit()
-    return {'message': 'success'}
+async def create_user(user_in: UserCreate) -> dict:
+    async with async_session() as session:
+        stmt = insert(User).values(**user_in.model_dump())
+        await session.execute(stmt)
+        await session.commit()
+        return {'message': 'success'}
 
 
 #READ
-async def get_user(user_email: str, session: AsyncSession = session_db) -> User | None:
-    stmt = select(User).where(User.email==user_email)
-    result = await session.execute(stmt)
-    user = result.scalars()
-    return user
+async def get_user(user_email: str) -> User | None:
+    async with async_session() as session:
+        stmt = select(User).where(User.email==user_email)
+        result = await session.execute(stmt)
+        user = result.scalars().all()
+        return user
