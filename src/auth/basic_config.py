7fda76cea13506ff26fea730c52116e47aa12ede
Mark <markbakalkinsochi3@gmail.com>
2024-11-10 15:19:58 +0300

    Created crud folder

--- src/auth/basic_config.py
+++ src/auth/basic_config.py
@@ -5,7 +5,7 @@ from fastapi import Request, HTTPException, status, Depends
 
 from datetime import timedelta, timezone, datetime
 
-from ..crud import get_user
+from ..crud.crud import get_user
 from ..config import settings
 
 
