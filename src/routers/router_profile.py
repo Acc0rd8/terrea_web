19baccf790aedc2ec0fff7681289ee5e030336e0
Mark <markbakalkinsochi3@gmail.com>
2024-11-30 11:50:54 +0300

    delete "result = ..."

--- src/routers/router_profile.py
+++ src/routers/router_profile.py
@@ -19,42 +19,35 @@ router = APIRouter(
 
 @router.post('/register')
 async def register_user(response: Response, user_data: UserCreate, user_service: Annotated[UserService, Depends(user_service)]) -> dict:
-    result = await ProfileConfig.register_new_user(response, user_data, user_service)
-    return result
+    return await ProfileConfig.register_new_user(response, user_data, user_service)
 
 
 @router.post('/login')
 async def authenticate_user(response: Response, user_data: UserAuth, user_service: Annotated[UserService, Depends(user_service)]) -> Token:
-    result = await ProfileConfig.user_authentication(response, user_data, user_service)
-    return result
+    return await ProfileConfig.user_authentication(response, user_data, user_service)
 
 
 @router.patch('/update_profile')
 async def update_user(response: Response, user_data: Annotated[User, Depends(UserManager.get_current_user)], user_data_update: UserUpdate, user_service: Annotated[UserService, Depends(user_service)]):
-    result = await ProfileConfig.update_current_user(response, user_data, user_data_update, user_service)
-    return result
+    return await ProfileConfig.update_current_user(response, user_data, user_data_update, user_service)
 
 
 @router.get('/me')
 @cache(expire=600)
 async def get_me(user_data: Annotated[User, Depends(UserManager.get_current_user)]) -> UserRead:
-    result = await ProfileConfig.get_user_me(user_data)
-    return result
+    return await ProfileConfig.get_user_me(user_data)
 
 
 @router.get('/@{username}')
 async def get_user(username: str, user_service: Annotated[UserService, Depends(user_service)]) -> UserRead:
-    result = await ProfileConfig.get_another_user(username, user_service)
-    return result
+    return await ProfileConfig.get_another_user(username, user_service)
 
 
 @router.post('/logout')
 async def logout_user(response: Response, user_data: Annotated[User, Depends(UserManager.get_current_user)], user_service: Annotated[UserService, Depends(user_service)]) -> dict:
-    result = await ProfileConfig.logout_current_user(response, user_data, user_service)
-    return result
+    return await ProfileConfig.logout_current_user(response, user_data, user_service)
 
 
 @router.delete('/delete_account')
 async def delete_user_account(response: Response, user_data: Annotated[User, Depends(UserManager.get_current_user)], user_service: Annotated[UserService, Depends(user_service)]) -> dict:
-    result = await ProfileConfig.delete_current_user(response, user_data, user_service)
-    return result
+    return await ProfileConfig.delete_current_user(response, user_data, user_service)
--- src/routers/router_project.py
+++ src/routers/router_project.py
@@ -22,26 +22,20 @@ router = APIRouter(
 
 @router.post('/create_project')
 async def create_project_app(project_create: ProjectCreate, user_data: Annotated[User, Depends(UserManager.get_current_user)], project_service: Annotated[ProjectService, Depends(project_service)]) -> dict:
-    result = await ProjectConfig.create_new_project(project_create, user_data, project_service)
-    return result
+    return await ProjectConfig.create_new_project(project_create, user_data, project_service)
 
 
 @router.get('/{project_name}')
 @cache(expire=600)
 async def get_some_project(project_name: str, user_data: Annotated[User, Depends(UserManager.get_current_user)], project_service: Annotated[ProjectService, Depends(project_service)]) -> ProjectRead:
-    result = await ProjectConfig.get_some_project_by_name(project_name, user_data, project_service)
-    return result
+    return await ProjectConfig.get_some_project_by_name(project_name, user_data, project_service)
 
 
-#TODO
 @router.delete('/{project_name}/delete')
 async def delete_project(project_name: str, user_data: Annotated[User, Depends(UserManager.get_current_user)], project_service: Annotated[ProjectService, Depends(project_service)]) -> dict:
-    result = await ProjectConfig.delete_current_project(project_name, user_data, project_service)
-    return result
+    return await ProjectConfig.delete_current_project(project_name, user_data, project_service)
 
 
 @router.post('/{project_name}/task/create')
 async def create_task_in_project(project_name: str, task_create: TaskCreate, user_data: Annotated[User, Depends(UserManager.get_current_user)], task_service: Annotated[TaskService, Depends(task_service)], project_service: Annotated[ProjectService, Depends(project_service)]) -> dict:
-    result = await ProjectConfig.create_task_in_current_project(project_name, task_create, user_data, task_service, project_service)
-    return result
-    
+    return await ProjectConfig.create_task_in_current_project(project_name, task_create, user_data, task_service, project_service)
