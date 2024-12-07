43e585da3b0bf469e9349eccd15aacc3a33f4bad
Mark <markbakalkinsochi3@gmail.com>
2024-12-07 13:24:39 +0300

    moved conftest

---
+++ src/conftest.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from typing import AsyncGenerator
+
+from src.database import get_async_session
+from src.config import settings
+from src.main import app
+
+
+engine_test = create_async_engine(settings.TEST_DATABASE_URL, echo=True)
+async_session_factory_test = async_sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=True)
+
+
+async def get_async_session_test() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_factory_test() as session:
+        try:
+            yield session
+        except Exception as e:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
+            
+app.dependency_overrides[get_async_session] = get_async_session_test
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,39 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from typing import AsyncGenerator
+from fastapi.testclient import TestClient
+from sqlalchemy import insert
+from httpx import AsyncClient
+import asyncio
+import pytest
 
-from src.database import get_async_session
-from src.config import settings
-from src.main import app
+from src.conftest import engine_test, async_session_factory_test
+from src.database import Base
+from src.models.model_role import Role
+from src.models.model_user import User
+from src.models.model_project import Project
+from src.models.model_task import Task
+from src.main import app as fastapi_app
 
 
-engine_test = create_async_engine(settings.TEST_DATABASE_URL, echo=True)
-async_session_factory_test = async_sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=True)
-
-
-async def get_async_session_test() -> AsyncGenerator[AsyncSession, None]:
+@pytest.fixture(scope='session', autouse=True)
+async def database_prepare():
+    async with engine_test.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+        
     async with async_session_factory_test() as session:
-        try:
-            yield session
-        except Exception as e:
-            session.rollback()
-            raise
-        finally:
-            session.close()
-            
-app.dependency_overrides[get_async_session] = get_async_session_test
+        stmt = insert(Role).values(id=1, name='user', permicions=[None])
+        await session.execute(stmt)
+        await session.commit()
+
+
+@pytest.fixture(scope='session')
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+    
+    
+@pytest.fixture(scope='function')
+async def ac():
+    async with AsyncClient(app=fastapi_app, base_url='http://test') as ac:
+        yield ac
