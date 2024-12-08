5e0ff9b2f0eae3913af031fc1e695e5d2cd81f89
Mark <markbakalkinsochi3@gmail.com>
2024-12-08 16:02:17 +0300

    Add CRUD tests for User

--- tests/unit_tests/test_profile_crud.py
+++ tests/unit_tests/test_profile_crud.py
@@ -1,9 +1,12 @@
 import pytest
 
 from src.schemas.role_schemas import RoleCreate, RoleUpdate
+from src.schemas.user_schemas import UserCreate, UserUpdate
 from src.repositories.role_service import RoleService
+from src.repositories.user_service import UserService
 
 
+#TODO
 @pytest.mark.usefixtures('clear_roles')
 class TestRoleCRUD:
     async def test_create_role(self, role_create: RoleCreate, role_service_test: RoleService):
@@ -11,7 +14,7 @@ class TestRoleCRUD:
         assert result == {'message': 'Role has been created'}
     
     @pytest.mark.parametrize('role_id, response', [
-        (2, "<Role: id = 2, name = test, permicions = ['None']>"),
+        (1, "<Role: id = 1, name = test, permicions = ['None']>"),
     ])
     async def test_read_role(self, role_create: RoleCreate, role_id: int, response: str, role_service_test: RoleService):
         await role_service_test.create_role(role_create)
@@ -19,7 +22,7 @@ class TestRoleCRUD:
         assert str(result) == response
         
     @pytest.mark.parametrize('role_id, response', [
-        (3, "<Role: id = 3, name = test_updated, permicions = ['updated']>"),
+        (1, "<Role: id = 1, name = test_updated, permicions = ['updated']>"),
     ])
     async def test_update_role(self, role_create: RoleCreate, role_update: RoleUpdate, role_id: int, response: str, role_service_test: RoleService):
         await role_service_test.create_role(role_create)
@@ -27,7 +30,7 @@ class TestRoleCRUD:
         assert str(result) == response
         
     @pytest.mark.parametrize('role_id, response', [
-        (4, {'message': 'Role has been deleted'})
+        (1, {'message': 'Role has been deleted'})
     ])
     async def test_delete_one_role(self, role_create: RoleCreate, role_id: int, response: str, role_service_test: RoleService):
         await role_service_test.create_role(role_create)
@@ -38,3 +41,63 @@ class TestRoleCRUD:
         await role_service_test.create_role(role_create)
         result = await role_service_test.delete_all_roles()
         assert result == {'message': 'All Roles have been deleted'}
+        
+
+#TODO
+@pytest.mark.usefixtures('clear_users', 'clear_roles')
+class TestUserCRUD:
+    async def test_create_user(self, role_create: RoleCreate, user_create: UserCreate, role_service_test: RoleService, user_service_test: UserService):
+        await role_service_test.create_role(role_create)
+        result = await user_service_test.create_user(user_create)
+        assert result == {'message': 'User has been created'}
+    
+    @pytest.mark.parametrize('user_email, response', [
+        ('test@example.com', f"<User: id = 1, username = test1, email = test@example.com, password = test1, role_id = 1, is_active = True>")
+    ])
+    async def test_get_user_by_email(self, role_service_test: RoleService, user_service_test: UserService, user_email: str, response: str, role_create: RoleCreate, user_create: UserCreate):
+        await role_service_test.create_role(role_create)
+        await user_service_test.create_user(user_create)
+        result = await user_service_test.get_user_by_email(user_email)
+        assert str(result) == response
+    
+    @pytest.mark.parametrize('user_id, response', [
+        (1, f"<User: id = 1, username = test1, email = test@example.com, password = test1, role_id = 1, is_active = True>")
+    ])
+    async def test_get_user_by_id(self, role_service_test: RoleService, user_service_test: UserService, user_id: str, response: str, role_create: RoleCreate, user_create: UserCreate):
+        await role_service_test.create_role(role_create)
+        await user_service_test.create_user(user_create)
+        result = await user_service_test.get_user_by_id(user_id)
+        assert str(result) == response
+    
+    @pytest.mark.parametrize('user_name, response', [
+        ('test1', f"<User: id = 1, username = test1, email = test@example.com, password = test1, role_id = 1, is_active = True>")
+    ])
+    async def test_get_user_by_name(self, role_service_test: RoleService, user_service_test: UserService, user_name: str, response: str, role_create: RoleCreate, user_create: UserCreate):
+        await role_service_test.create_role(role_create)
+        await user_service_test.create_user(user_create)
+        result = await user_service_test.get_user_by_name(user_name)
+        assert str(result) == response
+    
+    @pytest.mark.parametrize('user_email, response', [
+        ('test@example.com', f"<User: id = 1, username = test1_updated, email = testupdated@example.com, password = updated, role_id = 1, is_active = True>")
+    ])
+    async def test_update_user(self, role_service_test: RoleService, user_service_test: UserService, user_email: str, response: str, role_create: RoleCreate, user_create: UserCreate, user_update: UserUpdate):
+        await role_service_test.create_role(role_create)
+        await user_service_test.create_user(user_create)
+        result = await user_service_test.update_user(user_update, user_email)
+        assert str(result) == response
+    
+    @pytest.mark.parametrize('user_email, response', [
+        ('test@example.com', {'message': 'User has been deleted'})
+    ])
+    async def test_delete_one_user(self, role_service_test: RoleService, user_service_test: UserService, user_email: str, response: str, role_create: RoleCreate, user_create: UserCreate):
+        await role_service_test.create_role(role_create)
+        await user_service_test.create_user(user_create)
+        result = await user_service_test.delete_one_user(user_email)
+        assert result == response
+    
+    async def test_delete_all_users(self, role_service_test: RoleService, user_service_test: UserService, role_create: RoleCreate, user_create: UserCreate):
+        await role_service_test.create_role(role_create)
+        await user_service_test.create_user(user_create)
+        result = await user_service_test.delete_all_users()
+        assert result == {'message': 'All Users have been deleted'}
