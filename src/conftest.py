a4cd5795b7c140d711292035954668a15902a925
Mark <markbakalkinsochi3@gmail.com>
2024-12-08 11:53:02 +0300

    Add poolclass parameter in engine_test

--- src/conftest.py
+++ src/conftest.py
@@ -1,13 +1,14 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from typing import AsyncGenerator
+from sqlalchemy.pool import NullPool
 
 from src.database import get_async_session
 from src.config import settings
 from src.main import app
 
 
-engine_test = create_async_engine(settings.TEST_DATABASE_URL, echo=True)
-async_session_factory_test = async_sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=True)
+engine_test = create_async_engine(settings.TEST_DATABASE_URL, echo=True, poolclass=NullPool)
+async_session_factory_test = async_sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False, autoflush=False)
 
 
 async def get_async_session_test() -> AsyncGenerator[AsyncSession, None]:
