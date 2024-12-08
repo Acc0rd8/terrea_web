3c6981ca798b58b2d3b00ac7faf1c722a510b4df
Mark <markbakalkinsochi3@gmail.com>
2024-12-08 11:53:54 +0300

    Add fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from sqlalchemy import insert
+from sqlalchemy import insert, delete
 from httpx import AsyncClient
 import asyncio
 import pytest
@@ -11,6 +11,7 @@ from src.models.model_user import User
 from src.models.model_project import Project
 from src.models.model_task import Task
 from src.main import app as fastapi_app
+from src.schemas.role_schemas import RoleCreate
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -25,7 +26,25 @@ async def database_prepare():
         await session.commit()
 
 
-@pytest.fixture(scope='session')
+#TODO change using async_session_factory_test to async_session_generator(get_async_session_test)
+@pytest.fixture()
+async def clear_users():
+    async with async_session_factory_test() as session:
+        stmt = delete(User)
+        await session.execute(stmt)
+        await session.commit()
+
+
+#TODO change using async_session_factory_test to async_session_generator(get_async_session_test)
+@pytest.fixture()
+async def clear_roles():
+    async with async_session_factory_test() as session:
+        stmt = delete(Role)
+        await session.execute(stmt)
+        await session.commit()
+
+
+@pytest.fixture(scope='session', autouse=True)
 def event_loop(request):
     """Create an instance of the default event loop for each test case."""
     loop = asyncio.get_event_loop_policy().new_event_loop()
@@ -37,3 +56,17 @@ def event_loop(request):
 async def ac():
     async with AsyncClient(app=fastapi_app, base_url='http://test') as ac:
         yield ac
+
+
+#TODO Change session fixture to Service fixture
+@pytest.fixture(scope='function')
+async def session():
+    async with async_session_factory_test() as session:
+        yield session
+
+
+#TODO Add list of roles
+@pytest.fixture(scope='function')
+async def role_create():
+    role_create = RoleCreate(name='test', permicions=['None'])
+    return role_create
