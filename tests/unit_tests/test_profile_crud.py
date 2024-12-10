4ace6921eeebd77b35b4161421a0ebccabf32372
Mark <markbakalkinsochi3@gmail.com>
2024-12-10 11:01:07 +0300

    Update unit tests

--- tests/unit_tests/test_profile_crud.py
+++ tests/unit_tests/test_profile_crud.py
@@ -6,91 +6,123 @@ from src.repositories.role_service import RoleService
 from src.repositories.user_service import UserService
 
 
-#TODO
 class TestRoleCRUD:
     @pytest.mark.usefixtures('clear_roles')
-    async def test_create_role(self, role_create: RoleCreate, role_service_test: RoleService):
+    @pytest.mark.parametrize('response', [
+        ({'message': 'Role has been created'})
+    ])
+    async def test_create_role(self, response: dict, role_create: RoleCreate, role_service_test: RoleService):
         result = await role_service_test.create_role(role_create)
-        assert result == {'message': 'Role has been created'}
+        assert result == response
+    
     
     @pytest.mark.usefixtures('clear_roles', 'create_role')
     @pytest.mark.parametrize('role_id, response', [
         (1, "<Role: id = 1, name = test, permicions = ['None']>"),
+        (2, 'None'),
+        (3, 'None')
     ])
     async def test_read_role(self, role_id: int, response: str, role_service_test: RoleService):
         result = await role_service_test.get_role(role_id)
         assert str(result) == response
     
+    
     @pytest.mark.usefixtures('clear_roles', 'create_role')
     @pytest.mark.parametrize('role_id, response', [
         (1, "<Role: id = 1, name = test_updated, permicions = ['updated']>"),
+        (2, 'None'),
+        (3, 'None')
     ])
-    async def test_update_role(self, role_update: RoleUpdate, role_id: int, response: str, role_service_test: RoleService):
+    async def test_update_role(self, role_id: int, response: str, role_update: RoleUpdate, role_service_test: RoleService):
         result = await role_service_test.update_role(role_update, role_id)
         assert str(result) == response
+    
     
     @pytest.mark.usefixtures('clear_roles', 'create_role')
     @pytest.mark.parametrize('role_id, response', [
         (1, {'message': 'Role has been deleted'})
     ])
-    async def test_delete_one_role(self, role_id: int, response: str, role_service_test: RoleService):
+    async def test_delete_one_role(self, role_id: int, response: dict, role_service_test: RoleService):
         result = await role_service_test.delete_one_role(role_id)
         assert result == response
         
+        
     @pytest.mark.usefixtures('clear_roles', 'create_role')
-    async def test_delete_all_roles(self, role_service_test: RoleService):
+    @pytest.mark.parametrize('response', [
+        ({'message': 'All Roles have been deleted'})
+    ])
+    async def test_delete_all_roles(self, response: dict, role_service_test: RoleService):
         result = await role_service_test.delete_all_roles()
-        assert result == {'message': 'All Roles have been deleted'}
+        assert result == response
         
 
-#TODO
 class TestUserCRUD:
     @pytest.mark.usefixtures('clear_roles', 'create_role')
-    async def test_create_user(self, user_create: UserCreate, user_service_test: UserService):
+    @pytest.mark.parametrize('response', [
+        ({'message': 'User has been created'})
+    ])
+    async def test_create_user(self, response: dict, user_create: UserCreate, user_service_test: UserService):
         result = await user_service_test.create_user(user_create)
-        assert result == {'message': 'User has been created'}
+        assert result == response
+    
     
     @pytest.mark.usefixtures('clear_users', 'clear_roles', 'create_role', 'create_user')
     @pytest.mark.parametrize('user_email, response', [
-        ('test@example.com', f"<User: id = 1, username = test1, email = test@example.com, password = test1, role_id = 1, is_active = True>")
+        ('test@example.com', f'<User: id = 1, username = test1, email = test@example.com, password = test1, role_id = 1, is_active = True>'),
+        ('None@example.com', 'None'),
+        ('123', 'None')
     ])
-    async def test_get_user_by_email(self, user_service_test: UserService, user_email: str, response: str):
+    async def test_get_user_by_email(self, user_email: str, response: str, user_service_test: UserService):
         result = await user_service_test.get_user_by_email(user_email)
         assert str(result) == response
     
+    
     @pytest.mark.usefixtures('clear_users', 'clear_roles', 'create_role', 'create_user')
     @pytest.mark.parametrize('user_id, response', [
-        (1, f"<User: id = 1, username = test1, email = test@example.com, password = test1, role_id = 1, is_active = True>")
+        (1, f'<User: id = 1, username = test1, email = test@example.com, password = test1, role_id = 1, is_active = True>'),
+        (2, 'None'),
+        (3, 'None')
     ])
-    async def test_get_user_by_id(self, user_service_test: UserService, user_id: str, response: str):
+    async def test_get_user_by_id(self, user_id: str, response: str, user_service_test: UserService):
         result = await user_service_test.get_user_by_id(user_id)
         assert str(result) == response
     
+    
     @pytest.mark.usefixtures('clear_users', 'clear_roles', 'create_role', 'create_user')
     @pytest.mark.parametrize('user_name, response', [
-        ('test1', f"<User: id = 1, username = test1, email = test@example.com, password = test1, role_id = 1, is_active = True>")
+        ('test1', f'<User: id = 1, username = test1, email = test@example.com, password = test1, role_id = 1, is_active = True>'),
+        ('None', 'None'),
+        ('DROP TABLE user', 'None')
     ])
-    async def test_get_user_by_name(self, user_service_test: UserService, user_name: str, response: str):
+    async def test_get_user_by_name(self, user_name: str, response: str, user_service_test: UserService):
         result = await user_service_test.get_user_by_name(user_name)
         assert str(result) == response
     
+    
     @pytest.mark.usefixtures('clear_users', 'clear_roles', 'create_role', 'create_user')
     @pytest.mark.parametrize('user_email, response', [
-        ('test@example.com', f"<User: id = 1, username = test1_updated, email = testupdated@example.com, password = updated, role_id = 1, is_active = True>")
+        ('test@example.com', f'<User: id = 1, username = test1_updated, email = testupdated@example.com, password = updated, role_id = 1, is_active = True>'),
+        ('None@example.com', 'None'),
+        ('abc', 'None')
     ])
-    async def test_update_user(self, user_service_test: UserService, user_email: str, response: str, user_update: UserUpdate):
+    async def test_update_user(self, user_email: str, response: str, user_update: UserUpdate, user_service_test: UserService):
         result = await user_service_test.update_user(user_update, user_email)
         assert str(result) == response
+    
     
     @pytest.mark.usefixtures('clear_users', 'clear_roles', 'create_role', 'create_user')
     @pytest.mark.parametrize('user_email, response', [
         ('test@example.com', {'message': 'User has been deleted'})
     ])
-    async def test_delete_one_user(self, user_service_test: UserService, user_email: str, response: str):
+    async def test_delete_one_user(self, user_email: str, response: dict, user_service_test: UserService):
         result = await user_service_test.delete_one_user(user_email)
         assert result == response
     
+    
     @pytest.mark.usefixtures('clear_users', 'clear_roles', 'create_role', 'create_user')
-    async def test_delete_all_users(self, user_service_test: UserService):
+    @pytest.mark.parametrize('response', [
+        ({'message': 'All Users have been deleted'})
+    ])
+    async def test_delete_all_users(self, response: dict, user_service_test: UserService):
         result = await user_service_test.delete_all_users()
-        assert result == {'message': 'All Users have been deleted'}
+        assert result == response
--- tests/unit_tests/test_project_crud.py
+++ tests/unit_tests/test_project_crud.py
@@ -3,22 +3,23 @@ import pytest
 from src.schemas.project_schemas import ProjectCreate, ProjectUpdate
 from src.schemas.task_schemas import TaskCreate, TaskUpdate
 from src.repositories.project_service import ProjectService
+from src.repositories.task_service import TaskService
 
 
-#TODO
 class TestProjectCRUD:
     @pytest.mark.usefixtures('clear_users', 'clear_roles', 'create_role', 'create_user')
-    @pytest.mark.parametrize('user_id', [
-        (1)
+    @pytest.mark.parametrize('user_id, response', [
+        (1, {'message': 'Project has been created'})
     ])
-    async def test_create_project(self, project_create: ProjectCreate, user_id: int, project_service_test: ProjectService):
+    async def test_create_project(self, user_id: int, response: dict, project_create: ProjectCreate, project_service_test: ProjectService):
         result = await project_service_test.create_project(project_create, user_id)
-        assert result == {'message': 'Project has been created'}
+        assert result == response
 
 
     @pytest.mark.usefixtures('clear_projects', 'clear_users', 'clear_roles', 'create_role', 'create_user', 'create_project')
     @pytest.mark.parametrize('project_id, response', [
-        (1, f'<Project: id = 1, name = test>')
+        (1, f'<Project: id = 1, name = test>'),
+        (2, 'None')
     ])
     async def test_get_project_by_id(self, project_id: int, response: str, project_service_test: ProjectService):
         result = await project_service_test.get_project_by_id(project_id)
@@ -27,7 +28,8 @@ class TestProjectCRUD:
     
     @pytest.mark.usefixtures('clear_projects', 'clear_users', 'clear_roles', 'create_role', 'create_user', 'create_project')
     @pytest.mark.parametrize('project_name, response', [
-        ('test', f'<Project: id = 1, name = test>')
+        ('test', f'<Project: id = 1, name = test>'),
+        ('None', 'None')
     ])
     async def test_get_project_by_name(self, project_name: str, response: str, project_service_test: ProjectService):
         result = await project_service_test.get_project_by_name(project_name)
@@ -36,7 +38,8 @@ class TestProjectCRUD:
     
     @pytest.mark.usefixtures('clear_projects', 'clear_users', 'clear_roles', 'create_role', 'create_user', 'create_project')
     @pytest.mark.parametrize('project_id, response', [
-        (1, f'<Project: id = 1, name = test_updated>')
+        (1, f'<Project: id = 1, name = test_updated>'),
+        (2, 'None')
     ])
     async def test_update_project(self, project_id: int, response: str, project_update: ProjectUpdate, project_service_test: ProjectService):
         result = await project_service_test.update_project(project_update, project_id)
@@ -47,7 +50,7 @@ class TestProjectCRUD:
     @pytest.mark.parametrize('project_id, response', [
         (1, {'message': 'Project has been deleted'})
     ])
-    async def test_delete_one_project_by_id(self, project_id: int, response: str, project_service_test: ProjectService):
+    async def test_delete_one_project_by_id(self, project_id: int, response: dict, project_service_test: ProjectService):
         result = await project_service_test.delete_one_project_by_id(project_id)
         assert result == response
     
@@ -56,29 +59,63 @@ class TestProjectCRUD:
     @pytest.mark.parametrize('project_name, response', [
         ('test', {'message': 'Project has been deleted'})
     ])
-    async def test_delete_one_project_by_name(self, project_name: str, response: str, project_service_test: ProjectService):
+    async def test_delete_one_project_by_name(self, project_name: str, response: dict, project_service_test: ProjectService):
         result = await project_service_test.delete_one_project_by_name(project_name)
         assert result == response
     
     
     @pytest.mark.usefixtures('clear_projects', 'clear_users', 'clear_roles', 'create_role', 'create_user', 'create_project')
-    async def test_delete_all_projects(self, project_service_test: ProjectService):
+    @pytest.mark.parametrize('response', [
+        ({'message': 'All Projects have been deleted'})
+    ])
+    async def test_delete_all_projects(self, response: dict, project_service_test: ProjectService):
         result = await project_service_test.delete_all_projects()
-        assert result == {'message': 'All Projects have been deleted'}
+        assert result == response
 
-#TODO
+
 class TestTaskCRUD:
-    async def test_create_task(self):
-        pass
+    @pytest.mark.usefixtures('clear_projects', 'clear_users', 'clear_roles', 'create_role', 'create_user', 'create_project')
+    @pytest.mark.parametrize('project_id, customer_id, response', [
+        (1, 1, {'message': 'Task has been created'})
+    ])
+    async def test_create_task(self, project_id: int, customer_id: int, response: dict, task_create: TaskCreate, task_service_test: TaskService):
+        result = await task_service_test.create_task(task_create, project_id, customer_id)
+        assert result == response
     
-    async def test_get_task(self):
-        pass
     
-    async def test_update_task(self):
-        pass
+    @pytest.mark.usefixtures('clear_projects', 'clear_tasks', 'clear_users', 'clear_roles', 'create_role', 'create_user', 'create_project', 'create_task')
+    @pytest.mark.parametrize('task_id, response', [
+        (1, '<Task: id = 1, name = test, project_id = 1>'),
+        (2, 'None')
+    ])
+    async def test_get_task(self, task_id: int, response: str, task_service_test: TaskService):
+        result = await task_service_test.get_task(task_id)
+        assert str(result) == response
     
-    async def test_delete_one_task(self):
-        pass
     
-    async def test_delete_all_tasks(self):
-        pass
+    @pytest.mark.usefixtures('clear_projects', 'clear_tasks', 'clear_users', 'clear_roles', 'create_role', 'create_user', 'create_project', 'create_task')
+    @pytest.mark.parametrize('task_id, response', [
+        (1, '<Task: id = 1, name = test_updated, project_id = 1>'),
+        (2, 'None')
+    ])
+    async def test_update_task(self, task_id: int, response: str, task_update: TaskUpdate, task_service_test: TaskService):
+        result = await task_service_test.update_task(task_update, task_id)
+        assert str(result) == response
+    
+    
+    @pytest.mark.usefixtures('clear_projects', 'clear_tasks', 'clear_users', 'clear_roles', 'create_role', 'create_user', 'create_project', 'create_task')
+    @pytest.mark.parametrize('task_id, response', [
+        (1, {'message': 'Task has been deleted'})
+    ])
+    async def test_delete_one_task(self, task_id: int, response: dict, task_service_test: TaskService):
+        result = await task_service_test.delete_one_task(task_id)
+        assert result == response
+    
+    
+    @pytest.mark.usefixtures('clear_projects', 'clear_tasks', 'clear_users', 'clear_roles', 'create_role', 'create_user', 'create_project', 'create_task')
+    @pytest.mark.parametrize('response', [
+        ({'message': 'All Tasks have been deleted'})
+    ])
+    async def test_delete_all_tasks(self, response: dict, task_service_test: TaskService):
+        result = await task_service_test.delete_all_tasks()
+        assert result == response
