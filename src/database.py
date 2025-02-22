b20249df31ee986d31e9427c0d3da7e3d75b1540
Mark <markbakalkinsochi3@gmail.com>
2025-02-22 17:17:10 +0300

    #2/Commit

--- src/database.py
+++ src/database.py
@@ -8,10 +8,23 @@ from src.logger import logger
 
 
 class Base(DeclarativeBase):
+    """
+    Base Database model class
+    
+    repr_cols_num - amount columns to print
+    repr_cols - additional columns except 'repr_cols_num'
+
+    """    
     repr_cols_num = 3
     repr_cols = tuple()
     
     def __repr__(self) -> dict:
+        """
+        Show model in cmd
+        
+        Returns:
+            string: <'Class.name': 'column1, column2, ...'>
+        """        
         cols = []
         for index, col in enumerate(self.__table__.columns.keys()):
             if col in self.repr_cols or index < self.repr_cols_num:
@@ -20,11 +33,16 @@ class Base(DeclarativeBase):
     
     @classmethod
     def to_string(cls):
+        """
+        Tablename to string
+        Returns:
+            string: Class.tablename
+        """        
         return cls.__tablename__.capitalize()
 
 
-engine = create_async_engine(settings.DATABASE_URL, echo=False)
-async_session_factory = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False, autoflush=False)
+engine = create_async_engine(settings.DATABASE_URL, echo=False) # Creating engine for connection with database settings (DATABASE_URL), echo=True - show SQL transactions
+async_session_factory = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False, autoflush=False) # Async session for creating SQL transations, autoflush=False - don't auto commit
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
@@ -32,6 +50,9 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
         try:
             yield session
         except Exception as e:
+            '''
+            If any exceptions, there will be showing database info in Logs
+            '''
             msg = f'Database connection Error {e}'
             extra = {
                 'DB_USER': settings.DATABASE_INFO['DB_USER'],
@@ -41,7 +62,7 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
                 'DB_NAME': settings.DATABASE_INFO['DB_NAME'],
             }
             logger.critical(msg=msg, extra=extra, exc_info=False)
-            await session.rollback()
+            await session.rollback() # Rollback SQL transations
             raise
         finally:
-            await session.close()
+            await session.close() # Close session
