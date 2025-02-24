3f03e2fbbe5974fc7255d6bf438fdaaf1e75fa53
Mark <112967688+Acc0rd8@users.noreply.github.com>
2025-02-24 21:31:13 +0300

    #3/add comments to services
    * Add comments to profile_config
    
    * Add comments to project_config

--- src/services/profile_config.py
+++ src/services/profile_config.py
@@ -16,33 +16,50 @@ from src.logger import logger
 class ProfileConfig:
     @staticmethod
     async def register_new_user(response: Response, user_data: UserCreate, user_service: UserService) -> dict:
+        """
+        Register new User
+
+        Args:
+            response (Response): Response to User
+            user_data (UserCreate): User data Validation
+            user_service (UserService): User DAO service
+
+        Raises:
+            HTTPException: status - 409, User is trying to create existing account
+            HTTPException: status - 409, User is trying to take existing Username
+            HTTPException: status - 400, User input symbols are incorrect
+            HTTPException: status - 500, SERVER ERROR
+
+        Returns:
+            dict[str, str]: Successfull registration 
+        """        
         try:
-            user_exist = await user_service.get_user_by_email(user_data.email)
+            user_exist = await user_service.get_user_by_email(user_data.email) # Check if User is already exist (User, None)
             if user_exist:
                 msg = 'User already exists'
                 extra = user_data.model_dump()
-                logger.warning(msg=msg, extra=extra, exc_info=False)
+                logger.warning(msg=msg, extra=extra, exc_info=False)  # log
                 raise HTTPException(
                     status_code=status.HTTP_409_CONFLICT,
                     detail='User already exists'
                 )
             
-            username_exist = await user_service.get_user_by_name(user_data.username)
+            username_exist = await user_service.get_user_by_name(user_data.username) # Check if Username is already taken (User, None)
             if username_exist:
                 msg = 'Username is already taken'
                 extra = {'username': username_exist}
-                logger.warning(msg=msg, extra=extra)
+                logger.warning(msg=msg, extra=extra)  # log
                 raise HTTPException(
                     status_code=status.HTTP_409_CONFLICT,
                     detail='Username is already taken'
                 )
             
-            user_dict = user_data.model_dump()
-            if await Security.validate_schemas_data_user(user_dict):   
-                user_dict['password'] = PasswordManager().get_password_hash(user_data.password)
+            user_dict = user_data.model_dump()  # Converting Pydantic model (UserCreate) to dict
+            if await Security.validate_schemas_data_user(user_dict):    # Check User symbols
+                user_dict['password'] = PasswordManager().get_password_hash(user_data.password) # Hashing password
                 await user_service.create_user(UserCreate(**user_dict))
-                access_token = TokenManager.create_access_token({'sub': str(user_data.email)})
-                response.set_cookie(key='user_access_token', value=access_token, httponly=True)
+                access_token = TokenManager.create_access_token({'sub': str(user_data.email)})  # Creating Token
+                response.set_cookie(key='user_access_token', value=access_token, httponly=True) # Only HTTP
                 return {'message': 'Successful registration'}
             else:
                 raise HTTPException(
@@ -57,17 +74,35 @@ class ProfileConfig:
 
     @staticmethod
     async def user_authentication(response: Response, request: Request, user_data: UserAuth, user_service: UserService) -> Token:
+        """
+        User Login
+
+        Args:
+            response (Response): Response to User
+            request (Request): Request from User
+            user_data (UserAuth): User data Validation
+            user_service (UserService): User DAO service
+
+        Raises:
+            HTTPException: status - 409, A Logged-in User is trying to Login again
+            HTTPException: status - 401, Incorrect email or password
+            HTTPException: status - 401, Incorrect email or password
+            HTTPException: status - 500, SERVER ERROR
+
+        Returns:
+            Token: Token with access token and token_type (Rework on production)
+        """        
         try:
-            token = request.cookies.get('user_access_token')
-            if token:
+            token = request.cookies.get('user_access_token') # Get User cookie 'user_access_token' from request
+            if token:   # If token exists => User is already logged-in
                 msg = 'User is already login'
-                logger.warning(msg=msg)
+                logger.warning(msg=msg)  # log
                 raise HTTPException(
                     status_code=status.HTTP_409_CONFLICT,
                     detail='User is already login'
                 )
             
-            user = await user_service.get_user_by_email(user_data.email)
+            user = await user_service.get_user_by_email(user_data.email) # Searching for a User in the Database
             if user is None:
                 msg = 'User doesnt exist'
                 logger.warning(msg=msg)
@@ -75,23 +110,24 @@ class ProfileConfig:
                     status_code=status.HTTP_401_UNAUTHORIZED,
                     detail='Incorrect email or password'
                 )
-            user_model_check = UserAuth.model_validate(user)
+                
+            user_model_check = UserAuth.model_validate(user) # Converting SQLAlchemy model to Pydantic model (UserAuth)
             if user_data.email != user_model_check.email or (not PasswordManager().verify_password(user_data.password, user_model_check.password)):
                 msg = 'Incorrect email or password'
                 extra = {'email': user_data.email, 'password': user_data.password}
-                logger.warning(msg=msg, extra=extra)
+                logger.warning(msg=msg, extra=extra)  # log
                 raise HTTPException(
                     status_code=status.HTTP_401_UNAUTHORIZED,
                     detail='Incorrect email or password'
                 )
                 
-            user_model_update = UserUpdate.model_validate(user)
-            if not user_model_update.is_active:
+            user_model_update = UserUpdate.model_validate(user) # Converting SQLAlchemy model to Pydantic model (UserUpdate)
+            if not user_model_update.is_active: # If User account isn't active, change field 'is_active'
                 user_model_update.is_active = True
                 await user_service.update_user(user_model_update, user_model_update.email)
             
-            access_token = TokenManager.create_access_token({'sub': str(user_data.email)})
-            response.set_cookie(key='user_access_token', value=access_token, httponly=True)
+            access_token = TokenManager.create_access_token({'sub': str(user_data.email)}) # Creating access token with User email
+            response.set_cookie(key='user_access_token', value=access_token, httponly=True) # Creating cookie for User
             return Token(access_token=access_token, token_type='cookie')
         except SQLAlchemyError:
             raise HTTPException(
@@ -101,21 +137,37 @@ class ProfileConfig:
 
     @staticmethod
     async def update_current_user(response: Response, user_data: User, user_data_update: UserUpdate, user_service: UserService) -> UserRead:
+        """
+        Update User Account
+
+        Args:
+            response (Response): Response to User
+            user_data (User): User data (SQLAlchemy Model)
+            user_data_update (UserUpdate): User update data Validation
+            user_service (UserService): User DAO service
+
+        Raises:
+            HTTPException: status - 400, User input symbols are incorrect
+            HTTPException: status - 500, SERVER ERROR
+
+        Returns:
+            UserRead: Updated User data
+        """        
         try:
-            new_user_dict = user_data_update.model_dump()
+            new_user_dict = user_data_update.model_dump() # Converting Pydantic model (UserUpdate) to dict
             
-            if await Security.validate_schemas_data_user(new_user_dict):
-                new_user_dict['password'] = PasswordManager().get_password_hash(user_data_update.password)
+            if await Security.validate_schemas_data_user(new_user_dict): # Check User symbols
+                new_user_dict['password'] = PasswordManager().get_password_hash(user_data_update.password) # Hashing password
                 
-                new_user_data = await user_service.update_user(UserUpdate(**new_user_dict), user_data.email)
+                new_user_data: User = await user_service.update_user(UserUpdate(**new_user_dict), user_data.email) # Updating User
                 
                 #TODO May be create refresh_token?....
-                response.delete_cookie(key='user_access_token')
-                access_token = TokenManager.create_access_token({'sub': str(user_data_update.email)})
-                response.set_cookie(key='user_access_token', value=access_token, httponly=True)
+                response.delete_cookie(key='user_access_token') # Updating cookie
+                access_token = TokenManager.create_access_token({'sub': str(user_data_update.email)}) # Updating cookie
+                response.set_cookie(key='user_access_token', value=access_token, httponly=True) # Updating cookie
                 
-                new_user_model = UserRead.model_validate(new_user_data)
-                date = re.search(r'\d{4}-\d{2}-\d{2}', f'{new_user_model.registred_at}')
+                new_user_model = UserRead.model_validate(new_user_data) # Converting SQLAlchemy model to Pydantic model (UserRead)
+                date = re.search(r'\d{4}-\d{2}-\d{2}', f'{new_user_model.registred_at}') # Date type YYYY-MM-DD
                 new_user_model.registred_at = date[0]
                 return new_user_model
             else:
@@ -131,9 +183,21 @@ class ProfileConfig:
     
     @staticmethod
     async def get_user_me(user_data: User) -> UserRead:
+        """
+        Show current User profile
+
+        Args:
+            user_data (User): User data (SQLAlchemy Model)
+
+        Raises:
+            HTTPException: status - 500, SERVER ERROR
+
+        Returns:
+            UserRead: User data
+        """        
         try:
-            user_model = UserRead.model_validate(user_data)
-            date = re.search(r'\d{4}-\d{2}-\d{2}', f'{user_model.registred_at}')
+            user_model = UserRead.model_validate(user_data) # Converting SQLAlchemy model to Pydantic model (UserRead)
+            date = re.search(r'\d{4}-\d{2}-\d{2}', f'{user_model.registred_at}') # Date type YYYY-MM-DD
             user_model.registred_at = date[0]
             return user_model
         except SQLAlchemyError:
@@ -144,18 +208,34 @@ class ProfileConfig:
     
     @staticmethod
     async def get_another_user(username: str, user_service: UserService) -> UserRead:
+        """
+        Show another User profile
+
+        Args:
+            username (str): Another User username
+            user_service (UserService): User DAO service
+
+        Raises:
+            HTTPException: status - 404, User doesn't exist
+            HTTPException: status - 400, User input symbols are incorrect
+            HTTPException: status - 500, SERVER ERROR
+
+        Returns:
+            UserRead: User data
+        """        
         try:
-            if await Security.validate_path_data(username):
-                another_user = await user_service.get_user_by_name(username)
+            if await Security.validate_path_data(username): # Check User symbols
+                another_user = await user_service.get_user_by_name(username) # Searching for a User in the Database 
                 if another_user is None:
-                    msg = 'User doesnt exist'
-                    logger.warning(msg=msg)
+                    msg = "User doesn't exist"
+                    logger.warning(msg=msg)  # log
                     raise HTTPException(
                         status_code=status.HTTP_404_NOT_FOUND,
-                        detail='User doesnt exist :('
+                        detail="User doesn't exist :("
                     )
-                another_user_model = UserRead.model_validate(another_user)
-                date = re.search(r'\d{4}-\d{2}-\d{2}', f'{another_user.registred_at}')
+                    
+                another_user_model = UserRead.model_validate(another_user) # Converting SQLAlchemy model to Pydantic model (UserRead)
+                date = re.search(r'\d{4}-\d{2}-\d{2}', f'{another_user.registred_at}') # Date type YYYY-MM-DD
                 another_user_model.registred_at = date[0]
                 return another_user_model
             else:
@@ -171,11 +251,25 @@ class ProfileConfig:
 
     @staticmethod
     async def logout_current_user(response: Response, user_data: User, user_service: UserService) -> dict:
+        """
+        Current User Logout
+
+        Args:
+            response (Response): Response to User
+            user_data (User): User data (SQLAlchemy model)
+            user_service (UserService): User DAO service
+
+        Raises:
+            HTTPException: status - 500, SERVER ERROR
+
+        Returns:
+            dict[str, str]: User successfull logout 
+        """        
         try:
             response.delete_cookie(key='user_access_token')
-            user_model_update = UserUpdate.model_validate(user_data)
-            user_model_update.is_active = False
-            await user_service.update_user(user_model_update, user_model_update.email)
+            user_model_update = UserUpdate.model_validate(user_data) # Converting SQLAlchemy model to Pydantic model (UserUpdate)
+            user_model_update.is_active = False # Change model field to FALSE
+            await user_service.update_user(user_model_update, user_model_update.email) # Update User data
             return {'message': 'User successfully logged out'}
         except SQLAlchemyError:
             raise HTTPException(
@@ -185,10 +279,24 @@ class ProfileConfig:
 
     @staticmethod
     async def delete_current_user(response: Response, user_data: User, user_service: UserService) -> dict:
+        """
+        Delete User account
+
+        Args:
+            response (Response): Response to User
+            user_data (User): User data (SQLAlchemy model)
+            user_service (UserService): User DAO service
+
+        Raises:
+            HTTPException: status - 500, SERVER ERROR
+
+        Returns:
+            dict[str, str]: User account has been deleted
+        """        
         try:
             response.delete_cookie(key='user_access_token')
-            user_model_data = UserDelete.model_validate(user_data)
-            await user_service.delete_one_user(user_model_data.email)
+            user_model_data = UserDelete.model_validate(user_data) # Converting SQLAlchemy model to Pydantic model (UserDelete)
+            await user_service.delete_one_user(user_model_data.email) # Delete User from Database
             return {'message': 'User account has been deleted'}
         except SQLAlchemyError:
             raise HTTPException(
--- src/services/project_config.py
+++ src/services/project_config.py
@@ -15,13 +15,29 @@ from src.dependencies.security import Security
 class ProjectConfig:
     @staticmethod
     async def create_new_project(project_create: ProjectCreate, user_data: User, project_service: ProjectService) -> dict:
+        """
+        Create new Project
+
+        Args:
+            project_create (ProjectCreate): Project data Validation
+            user_data (User): User data (SQLAlchemy model)
+            project_service (ProjectService): Project DAO service
+
+        Raises:
+            HTTPException: status - 409, Project name is already taken
+            HTTPException: status - 400, User input symbols are incorrect
+            HTTPException: status - 500, SERVER ERROR
+
+        Returns:
+            dict[str, str]: Project has been created 
+        """        
         try:
-            project_create_dict = project_create.model_dump()
-            if await Security.validate_shemas_data_project(project_create_dict):
+            project_create_dict = project_create.model_dump() # Converting Pydantic model to dict
+            if await Security.validate_shemas_data_project(project_create_dict):    # Check User symbols
                 for project in user_data.projects:
-                    project_dict = ProjectRead.model_validate(project).model_dump()
+                    project_dict = ProjectRead.model_validate(project).model_dump() # 1. Converting SQLAlchemy model to Pydantic model (ProjectRead), 2. Converting Pydantic model to dict
                     if project_dict['name'] == project_create.name:
-                        logger.warning(msg='Project name is already taken', extra={'project_name': project_create.name})
+                        logger.warning(msg='Project name is already taken', extra={'project_name': project_create.name})  # log
                         raise HTTPException(
                             status_code=status.HTTP_409_CONFLICT,
                             detail='Project name is already taken. Please take new name.'
@@ -41,26 +57,45 @@ class ProjectConfig:
             
     @staticmethod
     async def get_some_project_by_name(project_name: str, user_data: User, project_service: ProjectService) -> ProjectRead:
+        """
+        Show another User Project
+
+        Args:
+            project_name (str): Project name
+            user_data (User): User data (SQLAlchemy model)
+            project_service (ProjectService): Project DAO service
+
+        Raises:
+            HTTPException: status - 404, Project doesn't exist
+            HTTPException: status - 405, Don't have enough access rights to see the project
+            HTTPException: status - 400, User input symbols are incorrect
+            HTTPException: status - 500, SERVER ERROR
+
+        Returns:
+            ProjectRead: Project data
+        """        
         try:
-            if Security.validate_path_data(project_name):
-                project = await project_service.get_project_by_name(project_name)
+            if Security.validate_path_data(project_name): # Check User symbold
+                project = await project_service.get_project_by_name(project_name) # Searching for a Project in the Database
                 if project is None:
-                    msg = 'Project doesnt exist'
-                    logger.warning(msg=msg)
+                    msg = "Project doesn't exist"
+                    logger.warning(msg=msg)  # log
                     raise HTTPException(
                         status_code=status.HTTP_404_NOT_FOUND,
-                        detail='Project doesnt exist'
+                        detail="Project doesn't exist"
                     )
-                if project.owner_id != user_data.id:
-                    msg = 'You dont have enough access rights to see this project'
+                    
+                if project.owner_id != user_data.id: # If User doesn't own the project
+                    msg = "You don't have enough access rights to see this project"
                     extra = {'project_owner_id': project.owner_id, 'user_data_id': user_data.id}
-                    logger.warning(msg=msg, extra=extra)
+                    logger.warning(msg=msg, extra=extra)  # log
                     raise HTTPException(
                         status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
-                        detail='You dont have enough access rights to see this project'
+                        detail="You don't have enough access rights to see this project"
                     )
-                project_model = ProjectRead.model_validate(project)
-                date = re.search(r'\d{4}-\d{2}-\d{2}', f'{project_model.created_at}')
+                    
+                project_model = ProjectRead.model_validate(project) # Converting SQLAlchemy model to Pydantic model (ProjectRead)
+                date = re.search(r'\d{4}-\d{2}-\d{2}', f'{project_model.created_at}') # Date type YYYY-MM-DD
                 project_model.created_at = date[0]
                 return project_model
             else:
@@ -76,24 +111,41 @@ class ProjectConfig:
     
     @staticmethod
     async def delete_current_project(project_name: str, user_data: User, project_service: ProjectService) -> dict:
+        """
+        Delete Project
+
+        Args:
+            project_name (str): Project name
+            user_data (User): User data (SQLAlchemy model)
+            project_service (ProjectService): Project DAO service
+
+        Raises:
+            HTTPException: status - 404, Project doesn't exist
+            HTTPException: status - 405, Don't have enough access rights to see the project
+            HTTPException: status - 400, User input symbols are incorrect
+            HTTPException: status - 500, SERVER ERROR
+
+        Returns:
+            dict[str, str]: Project has been deleted
+        """        
         try:
-            if Security.validate_path_data(project_name):
-                project = await project_service.get_project_by_name(project_name)
+            if Security.validate_path_data(project_name): # Check User symbols
+                project = await project_service.get_project_by_name(project_name) # Searching for a Project in the Database
                 if project is None:
-                    msg = 'Project doesnt exist'
-                    logger.warning(msg=msg)
+                    msg = "Project doesn't exist"
+                    logger.warning(msg=msg)  # log
                     raise HTTPException(
                         status_code=status.HTTP_404_NOT_FOUND,
-                        detail='Project doesnt exist'
+                        detail="Project doesn't exist"
                     )
                 
-                if project.owner_id != user_data.id:
-                    msg = 'You dont have enough access rights to see this project'
+                if project.owner_id != user_data.id: # If User doesn't own the project
+                    msg = "You don't have enough access rights to see this project"
                     extra = {'project_owner_id': project.owner_id, 'user_data_id': user_data.id}
-                    logger.warning(msg=msg, extra=extra)
+                    logger.warning(msg=msg, extra=extra)  # log
                     raise HTTPException(
                         status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
-                        detail='You dont have enough access rights to see this project'
+                        detail="You don't have enough access rights to see this project"
                     )
                     
                 await project_service.delete_one_project_by_name(project_name)
@@ -111,23 +163,43 @@ class ProjectConfig:
     
     @staticmethod
     async def create_task_in_current_project(project_name: str, task_create: TaskCreate, user_data: User, task_service: TaskService, project_service: ProjectService) -> dict:
+        """
+        Create Task in Project
+
+        Args:
+            project_name (str): Project name
+            task_create (TaskCreate): Task data Validation
+            user_data (User): User data (SQLAlcehmy model)
+            task_service (TaskService): Task DAO service
+            project_service (ProjectService): Project DAO service
+
+        Raises:
+            HTTPException: status - 404, Project doesn't exist
+            HTTPException: status - 405, Don't have enough access rights to see the project
+            HTTPException: status - 400, User input symbols are incorrect
+            HTTPException: status - 500, SERVER ERROR
+
+        Returns:
+            dict[str, str]: Task has been created
+        """        
         try:
-            if Security.validate_path_data(project_name) and Security.validate_schemas_data_task(task_create.model_dump()):
-                project = await project_service.get_project_by_name(project_name)
+            if Security.validate_path_data(project_name) and Security.validate_schemas_data_task(task_create.model_dump()): # Check User symbols
+                project = await project_service.get_project_by_name(project_name) # Searching for a Project in the Database
                 if project is None:
-                    msg = 'Project doesnt exist'
-                    logger.warning(msg=msg)
+                    msg = "Project doesn't exist"
+                    logger.warning(msg=msg)  # log
                     raise HTTPException(
                         status_code=status.HTTP_404_NOT_FOUND,
-                        detail='Project doesnt exist'
+                        detail="Project doesn't exist"
                     )
-                if project.owner_id != user_data.id:
-                    msg = 'You dont have enough access rights to see this project'
+                    
+                if project.owner_id != user_data.id: # If User doesn't own the project
+                    msg = "You don't have enough access rights to see this project"
                     extra = {'project_owner_id': project.owner_id, 'user_data_id': user_data.id}
-                    logger.warning(msg=msg, extra=extra)
+                    logger.warning(msg=msg, extra=extra)  # log
                     raise HTTPException(
                         status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
-                        detail='You dont have enough access rights to see this project'
+                        detail="You don't have enough access rights to see this project"
                     )
                     
                 await task_service.create_task(task_create, project.id, user_data.id)
