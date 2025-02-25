22182065d839cc02c5f55b7fea4aa3aa0a18ae42
Mark <markbakalkinsochi3@gmail.com>
2025-02-25 18:42:00 +0300

    Add comments to base_schema

--- src/schemas/base_schema.py
+++ src/schemas/base_schema.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel
 
 
 class BaseSchema(BaseModel):
-    model_config = {'extra': 'forbid'}
-    model_config = {'from_attributes': True}
+    model_config = {'extra': 'forbid'}  # Forbid Extra fields
+    model_config = {'from_attributes': True}  # Allow to Convert SQLAlchemy model to Pydantic model
