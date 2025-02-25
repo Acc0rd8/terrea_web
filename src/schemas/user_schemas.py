c36816ccdef6e112df6d2c88c9cbf8a04644460b
Mark <markbakalkinsochi3@gmail.com>
2025-02-25 18:58:04 +0300

    Add comments to user_schemas

--- src/schemas/user_schemas.py
+++ src/schemas/user_schemas.py
@@ -8,33 +8,33 @@ from src.schemas.project_schemas import ProjectRead
 from src.schemas.task_schemas import TaskRead
 
 
-class UserBase(BaseSchema):
+class UserBase(BaseSchema): # Base User Schema
     email: EmailStr
 
 
-class UserCreate(UserBase): 
-    username: str = Field(min_length=3, max_length=20) #Username (length >= 3 symbols) and (length <= 20 symbols)
-    password: str = Field(min_length=5) #Password (length >= 5 symbols)
+class UserCreate(UserBase): # Register
+    username: str = Field(min_length=3, max_length=20) # Username (length >= 3 symbols) and (length <= 20 symbols)
+    password: str = Field(min_length=5) # Password (length >= 5 symbols)
     
 
-class UserAuth(UserBase):
+class UserAuth(UserBase): # Login
     password: str
 
 
-class UserRead(UserBase):
+class UserRead(UserBase): # Show info about User
     username: str
     registred_at: Union[str, datetime]
     role_id: int
-    is_active: bool
-    projects: list[ProjectRead]
-    user_tasks: list[TaskRead]
+    is_active: bool # If User is active on site => is_active = True, else False
+    projects: list[ProjectRead] # List of User Project (Model ProjectRead)
+    user_tasks: list[TaskRead] # List of User Tasks (Model TaskRead)
 
 
-class UserUpdate(UserBase):
-    username: str = Field(min_length=3, max_length=20) #Username (length >= 3 symbols) and (length <= 20 symbols)
-    password: str = Field(min_length=5)
+class UserUpdate(UserBase): # Update
+    username: str = Field(min_length=3, max_length=20) # Username (length >= 3 symbols) and (length <= 20 symbols)
+    password: str = Field(min_length=5) # Password (length >= 5 symbols)
     is_active: bool = Field(default=True)
 
 
-class UserDelete(UserBase):
+class UserDelete(UserBase): # Delete
     pass
