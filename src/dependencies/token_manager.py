2ad361d273c4d0b8f2e8967786094c2d8761f9b8
Mark <112967688+Acc0rd8@users.noreply.github.com>
2025-02-26 18:34:30 +0300

    #8/Add comments to dependencies + some fixes

--- src/dependencies/token_manager.py
+++ src/dependencies/token_manager.py
@@ -16,7 +16,7 @@ class TokenManager:
         else:
             expire = datetime.now(timezone.utc) + timedelta(days=int(settings.ACCESS_TOKEN_EXPIRE_DAYS))
             
-        to_encode.update({'exp': expire})
+        to_encode.update({'exp': expire}) # Add expire to jwt
         auth_data = settings.AUTH_DATA
         encode_jwt = jwt.encode(to_encode, auth_data['secret_key'], algorithm=auth_data['algorithm'])
         return encode_jwt
@@ -24,8 +24,8 @@ class TokenManager:
     @staticmethod
     def get_access_token(request: Request) -> str:
         token = request.cookies.get('user_access_token')
-        if not token:
-            logger.warning(msg='Token not found')
+        if not token: # If User doesn't have a cookie 'user_access_token'
+            logger.warning(msg='Token not found') # log
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail={'message': 'Token not found', 'status_code': status.HTTP_401_UNAUTHORIZED}
--- src/dependencies/user_manager.py
+++ src/dependencies/user_manager.py
@@ -15,38 +15,54 @@ from src.logger import logger
 class UserManager:
     @staticmethod
     async def get_current_user(token: Annotated[str, Depends(TokenManager.get_access_token)], user_service: Annotated[UserService, Depends(user_service)]) -> User:
+        """
+        Check if User Logeed-in
+
+        Args:
+            token (Annotated[str, Depends): Dependencies with 'TokenManager.get_access_token'
+            user_service (Annotated[UserService, Depends): Dependencies with 'src.dependencies.model_service.user_service'. User DAO service
+
+        Raises:
+            HTTPException: status - 401, Token is invalid
+            HTTPException: status - 401, Token has expired
+            HTTPException: status - 401, User ID not found
+            HTTPException: status - 401, User not found
+
+        Returns:
+            User: User SQLAlchemy model
+        """
         try:
             auth_data = settings.AUTH_DATA
             payload = jwt.decode(token, auth_data['secret_key'], algorithms=auth_data['algorithm'])
         except JWTError:
-            logger.warning(msg='Token is invalid')
+            logger.warning(msg='Token is invalid') # log
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail={'message': 'Token is invalid', 'status_code' : status.HTTP_401_UNAUTHORIZED}
             )
             
-        expire = payload.get('exp')
-        expire_time = datetime.fromtimestamp(int(expire), tz=timezone.utc)
+        expire = payload.get('exp') # Take 'expire' from jwt
+        expire_time = datetime.fromtimestamp(int(expire), tz=timezone.utc) # Converting to datetime
         if not expire or (expire_time < datetime.now(timezone.utc)):
-            logger.warning(msg='Token expired', extra={'expire': expire})
+            logger.warning(msg='Token has expired', extra={'expire': expire}) # log
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
-                detail={'message': 'Токен истёк', 'status_code' : status.HTTP_401_UNAUTHORIZED}
+                detail={'message': 'Token has expired', 'status_code' : status.HTTP_401_UNAUTHORIZED}
             )
             
-        user_email = payload.get('sub')
+        user_email = payload.get('sub') # Take 'sub' from jwt
         if user_email is None:
-            logger.warning(msg='ID have not found', extra={'user_email': user_email})
+            logger.warning(msg='User ID not found', extra={'user_email': user_email}) # log
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
-                detail={'message': 'Не найден ID пользователя', 'status_code' : status.HTTP_401_UNAUTHORIZED}
+                detail={'message': 'User ID not found', 'status_code' : status.HTTP_401_UNAUTHORIZED}
             )
             
-        user = await user_service.get_user_by_email(user_email)
+        user = await user_service.get_user_by_email(user_email) # Searching User in the Database
         if user is None:
-            logger.warning(msg='User have not found')
+            logger.warning(msg='User not found') # log
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
-                detail={'message': 'Пользователь не найден', 'status_code' : status.HTTP_401_UNAUTHORIZED}
+                detail={'message': 'User not found', 'status_code' : status.HTTP_401_UNAUTHORIZED}
             )
         return user
