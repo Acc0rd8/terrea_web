f0b828fb9b137ccc1213159a94a5c3f17a56ba35
Mark <112967688+Acc0rd8@users.noreply.github.com>
2025-02-26 18:13:35 +0300

    Add comments to models

--- src/models/model_user.py
+++ src/models/model_user.py
@@ -6,20 +6,20 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.database import Base
 
 
-class User(Base):
+class User(Base): # User Table
     __tablename__ = 'user'
     
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    registred_at: Mapped[str] = mapped_column(TIMESTAMP, default=datetime.utcnow)
-    role_id: Mapped[int] = mapped_column(ForeignKey('role.id'), default=1)
-    is_active: Mapped[bool] = mapped_column(default=True, nullable=False)
+    registred_at: Mapped[str] = mapped_column(TIMESTAMP, default=datetime.utcnow) # Time when User was registred
+    role_id: Mapped[int] = mapped_column(ForeignKey('role.id'), default=1) # By default User has Role 'user'
+    is_active: Mapped[bool] = mapped_column(default=True, nullable=False) # If user is active at the site => is_active = True, else is_active = False
     
-    projects: Mapped[list['Project']] = relationship(back_populates='owner', order_by='asc(Project.name)', lazy='selectin')
-    assigned_user_tasks: Mapped[list['Task']] = relationship(back_populates='customer', order_by='asc(Task.name)', lazy='selectin', primaryjoin='User.id == Task.customer_id')
-    user_tasks: Mapped[list['Task']] = relationship(back_populates='performer', order_by='asc(Task.name)', lazy='selectin', primaryjoin='User.id == Task.performer_id')
+    projects: Mapped[list['Project']] = relationship(back_populates='owner', order_by='asc(Project.name)', lazy='selectin') # One2Many
+    assigned_user_tasks: Mapped[list['Task']] = relationship(back_populates='customer', order_by='asc(Task.name)', lazy='selectin', primaryjoin='User.id == Task.customer_id') # One2Many
+    user_tasks: Mapped[list['Task']] = relationship(back_populates='performer', order_by='asc(Task.name)', lazy='selectin', primaryjoin='User.id == Task.performer_id') # One2Many
     
     repr_cols_num = 4
     repr_cols = ('role_id', 'is_active', 'projects')
