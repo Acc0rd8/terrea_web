3e6e58ddff1490a22326778b84d70daf8a5b31b9
Mark <112967688+Acc0rd8@users.noreply.github.com>
2025-03-02 15:11:27 +0300

    #27/add comments to unit tests
    * Add comments to unit tests
    
    * Add test database info

--- tests/unit_tests/test_profile_crud.py
+++ tests/unit_tests/test_profile_crud.py
@@ -1,3 +1,5 @@
+# TEST Profile CRUD requests - TST1
+
 import pytest
 
 from src.schemas.role_schemas import RoleCreate, RoleUpdate
@@ -8,21 +10,31 @@ from src.repositories.user_service import UserService
 
 class TestRoleCRUD:
     @pytest.mark.usefixtures('clear_roles')
-    @pytest.mark.parametrize('response', [
-        ({'message': 'Role has been created'})
-    ])
-    async def test_create_role(self, response: dict, role_create: RoleCreate, role_service_test: RoleService):
+    async def test_create_role(self, role_create: RoleCreate, role_service_test: RoleService):
+        """ Test creating Role
+
+        Args:
+            role_create (RoleCreate): Role creating Validation
+            role_service_test (RoleService): Role DAO service
+        """
         result = await role_service_test.create_role(role_create)
-        assert result == response
+        assert result == {'message': 'Role has been created'}
     
     
     @pytest.mark.usefixtures('clear_roles', 'create_role')
     @pytest.mark.parametrize('role_id, response', [
         (1, "<Role: id = 1, name = test, permicions = ['None']>"),
         (2, 'None'),
-        (3, 'None')
+        (3, 'None'),
     ])
     async def test_read_role(self, role_id: int, response: str, role_service_test: RoleService):
+        """ Test reading Role from Database
+
+        Args:
+            role_id (int): Role id in the Database
+            response (str): test response
+            role_service_test (RoleService): Role DAO service
+        """
         result = await role_service_test.get_role(role_id)
         assert str(result) == response
     
@@ -34,6 +46,14 @@ class TestRoleCRUD:
         (3, 'None')
     ])
     async def test_update_role(self, role_id: int, response: str, role_update: RoleUpdate, role_service_test: RoleService):
+        """ Test updating Role
+
+        Args:
+            role_id (int): Role id in the Database
+            response (str): test response
+            role_update (RoleUpdate): Role updating Validation
+            role_service_test (RoleService): Role DAO service
+        """
         result = await role_service_test.update_role(role_update, role_id)
         assert str(result) == response
     
@@ -43,27 +63,39 @@ class TestRoleCRUD:
         (1, {'message': 'Role has been deleted'})
     ])
     async def test_delete_one_role(self, role_id: int, response: dict, role_service_test: RoleService):
+        """ Test deleting one Role
+
+        Args:
+            role_id (int): Role id in the Database
+            response (dict): test response
+            role_service_test (RoleService): Role DAO service
+        """
         result = await role_service_test.delete_one_role(role_id)
         assert result == response
         
         
     @pytest.mark.usefixtures('clear_roles', 'create_role')
-    @pytest.mark.parametrize('response', [
-        ({'message': 'All Roles have been deleted'})
-    ])
-    async def test_delete_all_roles(self, response: dict, role_service_test: RoleService):
+    async def test_delete_all_roles(self, role_service_test: RoleService):
+        """ Test deleting all Roles
+
+        Args:
+            role_service_test (RoleService): Role DAO service
+        """
         result = await role_service_test.delete_all_roles()
-        assert result == response
+        assert result == {'message': 'All Roles have been deleted'}
         
 
 class TestUserCRUD:
     @pytest.mark.usefixtures('clear_roles', 'create_role')
-    @pytest.mark.parametrize('response', [
-        ({'message': 'User has been created'})
-    ])
-    async def test_create_user(self, response: dict, user_create: UserCreate, user_service_test: UserService):
+    async def test_create_user(self, user_create: UserCreate, user_service_test: UserService):
+        """ Test creating User
+
+        Args:
+            user_create (UserCreate): User creating Validation
+            user_service_test (UserService): User DAO service
+        """
         result = await user_service_test.create_user(user_create)
-        assert result == response
+        assert result == {'message': 'User has been created'}
     
     
     @pytest.mark.usefixtures('clear_users', 'clear_roles', 'create_role', 'create_user')
@@ -73,6 +105,13 @@ class TestUserCRUD:
         ('123', 'None')
     ])
     async def test_get_user_by_email(self, user_email: str, response: str, user_service_test: UserService):
+        """ Test reading User by email from the Database
+
+        Args:
+            user_email (str): User email in the Database
+            response (str): test response
+            user_service_test (UserService): User DAO service
+        """
         result = await user_service_test.get_user_by_email(user_email)
         assert str(result) == response
     
@@ -84,6 +123,13 @@ class TestUserCRUD:
         (3, 'None')
     ])
     async def test_get_user_by_id(self, user_id: str, response: str, user_service_test: UserService):
+        """ Test reading User by id from the Database
+
+        Args:
+            user_id (str): User id in the Database
+            response (str): test response
+            user_service_test (UserService): User DAO service
+        """
         result = await user_service_test.get_user_by_id(user_id)
         assert str(result) == response
     
@@ -95,6 +141,13 @@ class TestUserCRUD:
         ('DROP TABLE user', 'None')
     ])
     async def test_get_user_by_name(self, user_name: str, response: str, user_service_test: UserService):
+        """ Test reading User by name from the Database
+
+        Args:
+            user_name (str): User name in the Database
+            response (str): test response
+            user_service_test (UserService): User DAO service
+        """
         result = await user_service_test.get_user_by_name(user_name)
         assert str(result) == response
     
@@ -106,6 +159,14 @@ class TestUserCRUD:
         ('abc', 'None')
     ])
     async def test_update_user(self, user_email: str, response: str, user_update: UserUpdate, user_service_test: UserService):
+        """ Test updating User
+
+        Args:
+            user_email (str): User email in the Database
+            response (str): test response
+            user_update (UserUpdate): User update Validation
+            user_service_test (UserService): User DAO service
+        """
         result = await user_service_test.update_user(user_update, user_email)
         assert str(result) == response
     
@@ -115,14 +176,23 @@ class TestUserCRUD:
         ('test@example.com', {'message': 'User has been deleted'})
     ])
     async def test_delete_one_user(self, user_email: str, response: dict, user_service_test: UserService):
+        """ Test deleting one User
+
+        Args:
+            user_email (str): User email in the Database
+            response (dict): test response
+            user_service_test (UserService): User DAO service
+        """
         result = await user_service_test.delete_one_user(user_email)
         assert result == response
     
     
     @pytest.mark.usefixtures('clear_users', 'clear_roles', 'create_role', 'create_user')
-    @pytest.mark.parametrize('response', [
-        ({'message': 'All Users have been deleted'})
-    ])
-    async def test_delete_all_users(self, response: dict, user_service_test: UserService):
+    async def test_delete_all_users(self, user_service_test: UserService):
+        """ Test deleting all Users
+
+        Args:
+            user_service_test (UserService): User DAO service
+        """
         result = await user_service_test.delete_all_users()
-        assert result == response
+        assert result == {'message': 'All Users have been deleted'}
--- tests/unit_tests/test_project_crud.py
+++ tests/unit_tests/test_project_crud.py
@@ -1,3 +1,5 @@
+# TEST Project CRUD requests - TST2 
+
 import pytest
 
 from src.schemas.project_schemas import ProjectCreate, ProjectUpdate
@@ -12,6 +14,14 @@ class TestProjectCRUD:
         (1, {'message': 'Project has been created'})
     ])
     async def test_create_project(self, user_id: int, response: dict, project_create: ProjectCreate, project_service_test: ProjectService):
+        """ Test creating Project
+
+        Args:
+            user_id (int): user id in the Database
+            response (dict): test response
+            project_create (ProjectCreate): Project create Validation
+            project_service_test (ProjectService): Project DAO service
+        """
         result = await project_service_test.create_project(project_create, user_id)
         assert result == response
 
@@ -22,6 +32,13 @@ class TestProjectCRUD:
         (2, 'None')
     ])
     async def test_get_project_by_id(self, project_id: int, response: str, project_service_test: ProjectService):
+        """ Test reading Project by id from the Database
+
+        Args:
+            project_id (int): Project id in the Database
+            response (str): test response
+            project_service_test (ProjectService): Project DAO service
+        """
         result = await project_service_test.get_project_by_id(project_id)
         assert str(result) == response
     
@@ -32,6 +49,13 @@ class TestProjectCRUD:
         ('None', 'None')
     ])
     async def test_get_project_by_name(self, project_name: str, response: str, project_service_test: ProjectService):
+        """ Test reading Project by name from the Database
+
+        Args:
+            project_name (str): Project name in the Database
+            response (str): test response
+            project_service_test (ProjectService): Project DAO service
+        """        
         result = await project_service_test.get_project_by_name(project_name)
         assert str(result) == response
     
@@ -42,6 +66,14 @@ class TestProjectCRUD:
         (2, 'None')
     ])
     async def test_update_project(self, project_id: int, response: str, project_update: ProjectUpdate, project_service_test: ProjectService):
+        """ Test updating Project
+
+        Args:
+            project_id (int): Project id in the Database
+            response (str): test response
+            project_update (ProjectUpdate): Project update Validation
+            project_service_test (ProjectService): Project DAO service
+        """
         result = await project_service_test.update_project(project_update, project_id)
         assert str(result) == response
     
@@ -51,6 +83,13 @@ class TestProjectCRUD:
         (1, {'message': 'Project has been deleted'})
     ])
     async def test_delete_one_project_by_id(self, project_id: int, response: dict, project_service_test: ProjectService):
+        """ Test deleting one Project by id
+
+        Args:
+            project_id (int): Project id in the Database
+            response (dict): test response
+            project_service_test (ProjectService): Project DAO service
+        """
         result = await project_service_test.delete_one_project_by_id(project_id)
         assert result == response
     
@@ -60,17 +99,26 @@ class TestProjectCRUD:
         ('test', {'message': 'Project has been deleted'})
     ])
     async def test_delete_one_project_by_name(self, project_name: str, response: dict, project_service_test: ProjectService):
+        """ Test deleting one Project by name
+
+        Args:
+            project_name (str): Project name in the Database
+            response (dict): test response
+            project_service_test (ProjectService): Project DAO service
+        """
         result = await project_service_test.delete_one_project_by_name(project_name)
         assert result == response
     
     
     @pytest.mark.usefixtures('clear_projects', 'clear_users', 'clear_roles', 'create_role', 'create_user', 'create_project')
-    @pytest.mark.parametrize('response', [
-        ({'message': 'All Projects have been deleted'})
-    ])
-    async def test_delete_all_projects(self, response: dict, project_service_test: ProjectService):
+    async def test_delete_all_projects(self, project_service_test: ProjectService):
+        """ Test deleting all Projects
+
+        Args:
+            project_service_test (ProjectService): Project DAO service
+        """
         result = await project_service_test.delete_all_projects()
-        assert result == response
+        assert result == {'message': 'All Projects have been deleted'}
 
 
 class TestTaskCRUD:
@@ -79,6 +127,15 @@ class TestTaskCRUD:
         (1, 1, {'message': 'Task has been created'})
     ])
     async def test_create_task(self, project_id: int, customer_id: int, response: dict, task_create: TaskCreate, task_service_test: TaskService):
+        """ Test creating Task
+
+        Args:
+            project_id (int): Project id in the Database
+            customer_id (int): User id in the Database
+            response (dict): test response
+            task_create (TaskCreate): Task create Validation
+            task_service_test (TaskService): Task DAO service
+        """
         result = await task_service_test.create_task(task_create, project_id, customer_id)
         assert result == response
     
@@ -89,6 +146,13 @@ class TestTaskCRUD:
         (2, 'None')
     ])
     async def test_get_task(self, task_id: int, response: str, task_service_test: TaskService):
+        """ Test reading Task from the Database
+
+        Args:
+            task_id (int): Task id in the Database
+            response (str): test response
+            task_service_test (TaskService): Task DAO service
+        """
         result = await task_service_test.get_task(task_id)
         assert str(result) == response
     
@@ -99,6 +163,14 @@ class TestTaskCRUD:
         (2, 'None')
     ])
     async def test_update_task(self, task_id: int, response: str, task_update: TaskUpdate, task_service_test: TaskService):
+        """ Test updating Task
+
+        Args:
+            task_id (int): Task id in the Database
+            response (str): test response
+            task_update (TaskUpdate): Task update Validation
+            task_service_test (TaskService): Task DAO service
+        """
         result = await task_service_test.update_task(task_update, task_id)
         assert str(result) == response
     
@@ -108,14 +180,23 @@ class TestTaskCRUD:
         (1, {'message': 'Task has been deleted'})
     ])
     async def test_delete_one_task(self, task_id: int, response: dict, task_service_test: TaskService):
+        """ Test deleting one Task
+
+        Args:
+            task_id (int): Task id in the Database
+            response (dict): test response
+            task_service_test (TaskService): Task DAO service
+        """
         result = await task_service_test.delete_one_task(task_id)
         assert result == response
     
     
     @pytest.mark.usefixtures('clear_projects', 'clear_tasks', 'clear_users', 'clear_roles', 'create_role', 'create_user', 'create_project', 'create_task')
-    @pytest.mark.parametrize('response', [
-        ({'message': 'All Tasks have been deleted'})
-    ])
-    async def test_delete_all_tasks(self, response: dict, task_service_test: TaskService):
+    async def test_delete_all_tasks(self, task_service_test: TaskService):
+        """ Test deleting all Tasks
+
+        Args:
+            task_service_test (TaskService): Task DAO service
+        """
         result = await task_service_test.delete_all_tasks()
-        assert result == response
+        assert result == {'message': 'All Tasks have been deleted'}
