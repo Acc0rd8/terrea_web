4de60846821feb5fcf42371ad24ec97be9b36e64
Mark <112967688+Acc0rd8@users.noreply.github.com>
2025-03-06 19:08:00 +0300

    #31/create email service
    * Add SMTP config
    
    * Add Celery task
    
    * Include celery task

---
+++ src/tasks/tasks.py
@@ -0,0 +1,25 @@
+from pydantic import EmailStr
+import smtplib
+
+from src.tasks.celery import app_celery
+from src.tasks.email_templates import create_register_confirmation_template
+from src.config import settings
+from src.logger import logger
+
+
+@app_celery.task
+def send_register_confirmation_email(email_to: EmailStr):
+    """ Send register email
+
+    Args:
+        email_to (EmailStr): User email
+    """
+    msg = create_register_confirmation_template(email_to)  # Creating email message template
+    try:
+        with smtplib.SMTP_SSL(settings.SMTP_HOST, settings.SMTP_PORT) as server:
+            server.login(settings.SMTP_USER, settings.SMTP_PASS)
+            server.send_message(msg)  # Sending message on User email
+    except smtplib.SMTPAuthenticationError as e:
+        msg = 'SMTPAuthenticationError'
+        extra = {'Error': e}
+        logger.critical(msg=msg, extra=extra)  # log
