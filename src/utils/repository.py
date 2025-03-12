9983fbeaf2318935afe377ce46418484006ffffa
Mark <markbakalkinsochi3@gmail.com>
2025-03-12 17:09:46 +0300

    Create Redis repos

--- src/utils/repository.py
+++ src/utils/repository.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 from sqlalchemy import delete, insert, select, update
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
+from redis import Redis
+from redis.exceptions import RedisError
 
 from src.database import Base
 from src.logger import logger
@@ -93,3 +95,82 @@ class SQLAlchemyRepository(AbstractRepository):
             await self.session.rollback()
             logger.critical(msg='SQLALCHEMY CRITICAL ERROR', extra={'Error': e}, exc_info=False) # log
             raise SQLAlchemyError
+        
+
+class RedisRepository(AbstractRepository):
+    data_type = None
+    
+    def __init__(self, RedisServer: Redis):
+        self.redis = RedisServer
+        
+    async def create_one(self, *data) -> dict:
+        try:
+            if self.data_type == 'string':
+                await self.redis.set(name=data[0], value=data[1])
+            elif self.data_type == 'hash':
+                await self.redis.hset(name=data[0], key=data[1], value=data[2])
+            else:
+                raise TypeError  #TODO
+            
+            return {'success': True}
+        except RedisError as e:
+            msg = 'REDIS CRITICAL ERROR'
+            extra = {'Error': e}
+            logger.critical(msg=msg, extra=extra, exc_info=False)  # log
+            raise e
+    
+    async def get_one(self, *data) -> str | dict:
+        try:
+            if self.data_type == 'string':
+                result: bytes = await self.redis.get(name=data[0])
+            elif self.data_type == 'hash':
+                result: bytes = await self.redis.hget(name=data[0], key=data[1])
+            else:
+                raise TypeError  #TODO
+            
+            if result:
+                return result.decode('utf-8')
+            return None
+        except RedisError as e:
+            msg = 'REDIS CRITICAL ERROR'
+            extra = {'Error': e}
+            logger.critical(msg=msg, extra=extra, exc_info=False)  # log
+            raise e
+    
+    async def update_one(self, *data) -> dict:  #TODO
+        try:
+            if self.data_type == 'string':
+                await self.redis.set(name=data[0], value=data[1])
+            elif self.data_type == 'hash':
+                await self.redis.hset(name=data[0], key=data[1], value=data[2])
+            else:
+                raise TypeError  #TODO
+            
+            return {'success': True}
+        except RedisError as e:
+            msg = 'REDIS CRITICAL ERROR'
+            extra = {'Error': e}
+            logger.critical(msg=msg, extra=extra, exc_info=False)  # log
+            raise e
+    
+    async def delete_one(self, *data) -> dict:
+        try:
+            await self.redis.delete(data)
+            
+            return {'success': True}
+        except RedisError as e:
+            msg = 'REDIS CRITICAL ERROR'
+            extra = {'Error': e}
+            logger.critical(msg=msg, extra=extra, exc_info=False)  # log
+            raise e
+    
+    async def delete_all(self) -> dict:
+        try:
+            await self.redis.flushdb(asynchronous=True)
+            
+            return {'success': True}
+        except RedisError as e:
+            msg = 'REDIS CRITICAL ERROR'
+            extra = {'Error': e}
+            logger.critical(msg=msg, extra=extra, exc_info=False)  # log
+            raise e
