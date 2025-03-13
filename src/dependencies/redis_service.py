2ca52239c83465875d490e0c5ea8f77743c4503c
Mark <markbakalkinsochi3@gmail.com>
2025-03-13 18:22:11 +0300

    Change connection

--- src/dependencies/redis_service.py
+++ src/dependencies/redis_service.py
@@ -1,11 +1,12 @@
+from redis.asyncio import Redis
+
 from src.redis_repositories.redis_string_type_service import RedisStringTypeService
 from src.redis_repositories.redis_hash_type_service import RedisHashTypeService
 from src.utils.redis_repos import RedisStringTypeRepository, RedisHashTypeRepository
-from src.redis_config import RedisServer
 
 
-def redis_string_type_service():
-    return RedisStringTypeService(RedisStringTypeRepository(RedisServer))
+def redis_string_type_service(connection: Redis):
+    return RedisStringTypeService(RedisStringTypeRepository(connection))
 
-def redis_hash_type_service():
-    return RedisHashTypeService(RedisHashTypeRepository(RedisServer))
+def redis_hash_type_service(connection: Redis):
+    return RedisHashTypeService(RedisHashTypeRepository(connection))
